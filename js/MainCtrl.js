429cb53d91948ca7e12ab222cc6b3d4f96a1a627
Aaron Endsley <aaronendsley@gmail.com>
2016-02-21 09:02:13 -0600

    added the parseAmount variable and the totalAmount Variable

--- js/MainCtrl.js
+++ js/MainCtrl.js
@@ -12,17 +12,29 @@ app.controller('MainCtrl', ['$scope', function($scope){
     $scope.feedback='Hello Welcome to the App, Please Add a bill and an amount and hit ';
     $scope.billsArray=[];
     $scope.amountArray=[];
+    $scope.parseAmount;
+    $scope.totalAmount=0;
+
 
 
 
     $scope.submit=function(){
+        $scope.parseAmount=parseFloat($scope.amount);
+        $scope.totalAmount= $scope.totalAmount + $scope.parseAmount;
+        $scope.totalAmount= $scope.totalAmount.toFixed(2);//Debating on moving this to another function
+        $scope.totalAmount=parseFloat($scope.totalAmount);// Above comment
         $scope.billsArray.push($scope.bill);
         $scope.amountArray.push($scope.amount);
         if($scope.billsArray.length > 0){
             $scope.feedback='Please feel free to input more items'
         }
-        $scope.bill=null;
-        $scope.amount=null;
+        function clearInput() {
+            $scope.bill = null;
+            $scope.amount = null;
+        }
+
+        clearInput()
+
     }
 
 
